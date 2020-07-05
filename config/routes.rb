af10ee55c46c095faa132518dccd651359c3b818
Randy <randyisbusy@gmail.com>
2020-07-05 09:21:07 -0700

    adds namespaces controller action and route api/v1/providers_controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,10 @@ Rails.application.routes.draw do
   resources :providers
   resources :patients
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+namespace :api do 
+    namespace :v1 do 
+     resources :providers, only: [:index, :create, :destroy, :update]
+    end 
+  end 
+
 end
